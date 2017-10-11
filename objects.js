fc66f992619c15791a054fa816e8662f1a92e497
Xochitl Fierros <xochitlfm@gmail.com>
2017-10-11 22:51:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,3 +6,9 @@ function updatePlaylist(playlist, artistName, songTitle){
 
   playlist[artistName] = songTitle
 }
+
+funtion removeFromPlaylist(playlist, artistName, songTitle) {
+  delete playlist[artistName]
+
+return playlist;
+}
